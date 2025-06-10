7a4b0ccc94009d2382fdf98df7b77e150ba2a88a
Ignacio Cortes Garcia <ncg2392@gmail.com>
2025-06-10 10:50:01 +0200

    feat: add logging middleware to track HTTP requests

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, _res, next) => {
+  console.log(`${req.method}: ${req.path}`);
+
+  next();
+});
+
 app.get('/health', (_req, res) => {
   res.status(200).json({ status: 'ok' });
 });
